b993455139a772dc90b1d9802f01ad86cecf6fb7
Vinicius Veronese Tufolo <vinicius.tufolo@terraviva.agr.br>
2022-09-23 13:10:06 -0300

    Footer

---
+++ src/assets/components/footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from './style'
+
+export default function Footer() {
+  return(
+    <Container>
+      <h2>footer</h2>
+    </Container>
+  )
+}
--- src/assets/pages/Main/index.jsx
+++ src/assets/pages/Main/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header'
+import Footer from '../../components/footer'
 
 export default function Main() {
   return (
     <>
       <Header />
+      <Footer/>
     </>
   )
 }
