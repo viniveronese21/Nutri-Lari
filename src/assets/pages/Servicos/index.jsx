b7839645afc4b9fab007abc43ce6b38c06d4bde9
viniveronese21 <vinicius21veronese@gmail.com>
2022-09-25 15:13:46 -0300

    Services

--- src/assets/pages/Servicos/index.jsx
+++ src/assets/pages/Servicos/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import { ContentText, MainContainer, Container, Title, ImgContainer, Img, Span} from './style'
+import Img1 from '../../img/kiwi.png'
+import Img2 from '../../img/abacaxi.png'
+import Img3 from '../../img/kiwi2.png'
 
 
 export default function Servicos() {
@@ -8,7 +12,38 @@ export default function Servicos() {
     <>
       <Header/>
 
-      <h1>Serviços</h1>
+      <MainContainer>
+
+        <Container>
+
+          <ContentText>
+
+              <Title>Por que ir ao nutricionista?</Title>
+
+              <Span>
+                <strong>Cuidar</strong> da sua alimentação afeta diretamente na sua saúde,
+                ter habitos alimentares saudáveis traz qualidade de vida. Uma melhor alimentação
+                traz benefícios tanto no presente quanto para o futuro. <strong>Ame-se</strong>.
+              </Span>
+              <br/>
+              <br/>
+              <Span>
+              Um <strong>nutricionista</strong> vai te guiar por onde começar e te dar as melhores maneiras
+              específicas para você. Te dará o caminho das pedras da<br/> boa alimentação.
+              </Span>
+
+          </ContentText>
+
+          <ImgContainer>
+              <Img src={Img1}/>
+              <Img src={Img2}/>
+              <Img src={Img3}/>
+          </ImgContainer>
+
+        </Container>
+
+      </MainContainer>
+
 
       <Footer/>
     </>
---
+++ src/assets/pages/Servicos/style.jsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+import banner from '../../img/Sbackground.png'
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 1070px;
+  background-image: url(${banner});
+  background-repeat: no-repeat;
+  background-size: 100%;
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1349px;
+  height: 1070px;
+  margin: 0 auto;
+
+`
+
+export const ContentText = styled.div `
+  width: 765px;
+  height: 563px;
+  margin: 138px 0 0 211px;
+
+`
+export const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 96px;
+`
+
+export const Span = styled.span`
+    font-size: 20px;
+    line-height: 40px;
+`
+
+export const ImgContainer = styled.div`
+  width: 967px;
+  height: 369px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+export const Img = styled.img`
+  width: 272px;
+  height: 369px;
+`
