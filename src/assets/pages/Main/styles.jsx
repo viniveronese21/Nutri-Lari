8f8e3becca1aa13e87b74e59fe1d6bc60db4feec
Lucas Dias <lucasdias067@gmail.com>
2022-09-25 12:12:53 -0300

    homepage

--- src/assets/pages/Main/styles.jsx
+++ src/assets/pages/Main/styles.jsx
@@ -20,15 +20,18 @@ gap: 9vw;
 
   div{
     padding: 10px;
-    font-size: 20px;
+    font-size: 19px;
     line-height: 20px;
     width: 300px;
+
   }
   
   strong {
     display: block;
+    font-size: 21px;
     margin-top: 40px;
     margin-bottom: 15px;
   }
+
 
 `
