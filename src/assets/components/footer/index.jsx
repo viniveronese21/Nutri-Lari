c75b9ca9f730d1dd64526e551bad63ec1455879f
Vinicius Veronese Tufolo <vinicius.tufolo@L363.terraviva.com.br>
2022-10-17 12:12:02 -0300

    icon-set

--- src/assets/components/footer/index.jsx
+++ src/assets/components/footer/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Container, Colum, Img, Span, Social, SocialIcons} from './style'
+import { Container, Colum, Img, Span, Social, SocialIcons } from './style'
 import { HiOutlineMail, HiOutlinePhone } from 'react-icons/hi'
-import { SiInstagram, SiWhatsapp} from 'react-icons/si'
-import Icon from '../../img/Icon.png'
+import { SiInstagram, SiWhatsapp } from 'react-icons/si'
+import Icon from '../../img/icon.png'
 
 export default function Footer() {
-  return(
+  return (
     <Container>
       <Colum>
 
@@ -14,11 +14,11 @@ export default function Footer() {
           <h3>ENTRE EM CONTATO</h3>
         </Span>
 
-        <Span><HiOutlineMail size={25}/>
+        <Span><HiOutlineMail size={25} />
           <a href="mailto:larissalongobardinutri@gmail.com">larissalongobardinutri@gmail.com</a>
         </Span>
 
-        <Span><HiOutlinePhone size={25}/>
+        <Span><HiOutlinePhone size={25} />
           <a href="tel:+5519998731628">(19) 9 9873-1628</a>
         </Span>
 
@@ -28,8 +28,8 @@ export default function Footer() {
         <Social>
           <h3>REDES SOCIAIS</h3>
           <SocialIcons>
-            <SiInstagram size={30}/>
-            <SiWhatsapp size={30}/>
+            <SiInstagram size={30} />
+            <SiWhatsapp size={30} />
           </SocialIcons>
         </Social>
       </Colum>
